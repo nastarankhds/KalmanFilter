d82b4e1773f8d6f6e9c8095937f02a5d6a0393a3
Nastaran Khoshnood Sarabi <139873732+nastarankhds@users.noreply.github.com>
2025-10-27 10:09:41 +0100

    Update prediction.m

--- prediction.m
+++ prediction.m
@@ -2,12 +2,15 @@ function [x_pred,p_pred] = prediction (x,p,A,B,delta,Q)
 
 dt = 0.01;
 
+% Continuous time system
 F = expm(A*dt);
 G = F*(eye(2) - expm(-A*dt)).*inv(A)*B;
 x_pred = F*x  +G*delta ;
 p_pred = F*p .*F' + Q;
 
+% discrete time system
 % x_pred = A*x  +B*delta ;
 % p_pred = A*p *A' + Q;
+
 
 end
